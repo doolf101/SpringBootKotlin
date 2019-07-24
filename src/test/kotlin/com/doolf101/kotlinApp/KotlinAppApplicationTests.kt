5530fdd9c027c62cd1f4b11d202a4bf3065c7d91
Ciaran Flood <flood.ciaran@gmail.com>
2019-07-24 23:57:21 +0100

    Initial Commit

---
+++ src/test/kotlin/com/doolf101/kotlinApp/KotlinAppApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.doolf101.kotlinApp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinAppApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
